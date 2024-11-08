45e7e45de33d4a05d53b26338a784ccaf70d979e
raj-jadhav <jadxur@gmail.com>
2024-11-08 12:19:05 +0300

    Learn about Variables and how to assign them and also did operations with them

---
+++ prog1_variables.c
@@ -0,0 +1,24 @@
+//Variables and statements
+//variables hold values
+
+#include <stdio.h>
+
+
+int main(void){
+/*
+//void means no values or null
+int i; // holds signed integers,meaning it can store both positive and negative values 
+float f;// holds signed floating point numbers
+
+    printf("Hello , this is my first program in C.\n");
+*/
+
+//Assigning variables values and also doing operations
+int x,y,z;
+x = 2;
+y = 5;
+z = (x*y)/x;
+
+    printf("\n the answer is %d\n", z);
+return 0;
+}
